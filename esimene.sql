aa9da02558d9683ab4548164263d10bb4034861f
Yaroslav Baka <jaroslavbaka25@gmail.com>
2026-02-02 16:05:42 +0200

    Update opilane table structure and insert data
    Added unique constraint to 'perenimi' and modified date column name to 'synniaeg'. Inserted sample data into 'opilane' table.

--- esimene.sql
+++ esimene.sql
@@ -5,8 +5,19 @@ use bakaSQL;
 CREATE TABLE opilane(
 opilaneID int PRIMARY KEY identity(1,1),
 eesnimi varchar(25),
-perenimi varchar(30) NOT null,
-synnieag date,
+perenimi varchar(30) NOT null UNIQUE,
+synniaeg date,
 aadress TEXT,
 kas_opib bit);
-SELECT * FROM opilane;
+SELECT * FROM opilane; -- kuvab tabeli
+
+-- tabeli kustutamine 
+DROP TABLE opilane ;
+
+
+-- andmete lisamine tabelisse opilane
+INSERT INTO opilane(eesnimi, perenimi, synniaeg, kas_opib)
+VALUES ('Nikita', 'Grossholm','2025-12-12', 1),
+('Oleg', 'Berezevski', '2025-05-12', 1),
+('Kirill', 'Fedulin', '2025-05-10', 0);
+Select * from opilane;
